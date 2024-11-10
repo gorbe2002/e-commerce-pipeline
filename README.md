7c976b3314332af9d49b988b9212fc4709d3c7d6
gorbe2002 <gorbe54@hotmail.com>
2024-11-10 17:20:07 -0500

    updated README.md and made kafka script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# e-commerce-pipeline
+# E-commerce Pipeline
 This is an ETL pipeline with random data to simulate e-commerce logs.
+
+# Steps
+Before running kafkaScript.py, make sure to do the following:
+- $ pip install kafka-python
+- Set up environment using Zookeeper (sample steps: https://kafka.apache.org/quickstart)
+    - Start Zookeeper: $ bin/zookeeper-server-start.sh config/zookeeper.properties
+    - Start Kafka: $ bin/kafka-server-start.sh config/server.properties
+    - Create "ecommerce-events": $ bin/kafka-topics.sh --create --topic ecommerce-events --bootstrap-server localhost:9092
+
+# Useful Links
+Learning about Apache Kafka:
+- https://kafka.apache.org/intro
+- https://kafka.apache.org/quickstart
+
+Kafka-Python library:
+- https://kafka-python.readthedocs.io/en/master/#
---
+++ kafkaScript.py
@@ -0,0 +1,33 @@
+"""
+Gabriel Orbe
+Last Updated: Nov. 10, 2024
+
+This file creates random data to emulate an e-commerce website and loads it to a Kafka topic
+"""
+
+from kafka import KafkaProducer
+import json
+import time
+import random
+
+# serialize json messages (KafkaProducer expects message data to be in bytes format)
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serialize=lambda v: json.dumps(v).encode('utf-8'))
+
+# example product types and actions by customers
+productTypes = ["clothing", "shoes", "electronics", "food", "beverages", "candles", "skincare"]
+actions = ["view", "addToCard", "buy"]
+
+# set up script to run for 10 minutes
+startTime = time.time()
+duration = 600 # 600 sec = 10 min
+
+# create an event every 10 seconds 
+while time.time() - startTime < duration:
+    event = {
+        "userId": random.randint(1, 100),
+        "productId": random.choice(productTypes),
+        "action": random.choice(actions),
+        "timestamp": time.time()
+    }
+    producer.send('ecommerce-events', event)
+    time.sleep(10)
